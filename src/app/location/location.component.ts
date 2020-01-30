27a0a3a5726eca444b4adde67e07de3d8ffcad07
joeymalouf <joeymalouf@gmail.com>
2020-01-29 19:31:26 -0600

    fixed trip advisor widget in a bad way; added menu layout

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent {
+  constructor() {
+  }
+
+
 }
+
